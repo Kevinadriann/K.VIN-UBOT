385595d59714e2f40bf3c94b83820571e3e65fd2
izzy <50057075+hitokizzy@users.noreply.github.com>
2022-02-21 13:22:11 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -19,6 +19,7 @@ from pySmartDL import SmartDL
 from pymongo import MongoClient
 from datetime import datetime
 from redis import StrictRedis
+from pytgcalls import pytgcalls
 from markdown import markdown
 from dotenv import load_dotenv
 from requests import get
